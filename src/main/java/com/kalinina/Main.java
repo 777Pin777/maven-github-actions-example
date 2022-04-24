3a3a5690c5ad61c586dfc07bacce23ff39bf51b1
Kalinina Mariya <kalinina.mariya777@gmail.com>
2022-04-24 11:56:16 +0300

    Add github actins

--- src/main/java/com/kalinina/Main.java
+++ src/main/java/com/kalinina/Main.java
@@ -1,4 +1,11 @@
 package com.kalinina;
 
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("Ура, мы подключили Git hub actions");
+        varz ;=1;
+
+    }
 }
